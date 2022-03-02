1a04bf7ad81609ea5df1248f3d6c55697074c5ca
User <RiyanChoudhury@riyans-mbp.lan>
2022-03-01 19:04:37 -0500

    added project

---
+++ TestNg/src/test/java/com/bit/test/SmokeTest.java
@@ -0,0 +1,17 @@
+package com.bit.test;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class SmokeTest extends BaseTest {
+
+	@Test
+	public void Test2() {
+		System.out.println("Test2ss");
+	}
+	@Test
+	public void Test3() {
+		System.out.println("Test3");
+	}
+
+}
